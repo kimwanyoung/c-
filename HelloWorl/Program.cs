d04555d8c88da19dcb052c0a403656ad9dd917f6
Wanyoung <78637786+kimwanyoung@users.noreply.github.com>
2023-01-01 21:33:38 +0900

    c# 클래스 구문

--- HelloWorl/Program.cs
+++ HelloWorl/Program.cs
@@ -31,7 +31,7 @@ foreach (var item in items)
 }
 */
 
-string[] stringArr = { "wan", "12", "young", "154", "13" };
+/* string[] stringArr = { "wan", "12", "young", "154", "13" };
 int sum = 0;
 string name = "";
 foreach(string value in stringArr)
@@ -48,3 +48,29 @@ foreach(string value in stringArr)
 
 Console.WriteLine(name);
 Console.WriteLine(sum);
+*/
+
+using System;
+
+namespace MyNewApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string value = "Microsoft Learn";
+            string reversedValue = Utility.Reverse(value);
+            Console.WriteLine($"Secret message: {reversedValue}");
+        }
+    }
+
+     class Utility
+     {
+        public static string Reverse(string message)
+        {
+            char[] letters = message.ToCharArray();
+            Array.Reverse(letters);
+            return new string(letters);
+        }
+    }
+}
