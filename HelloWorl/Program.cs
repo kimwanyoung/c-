ed93634c52740127dc6a812b0aa0d00e99e199ef
Wanyoung <78637786+kimwanyoung@users.noreply.github.com>
2023-01-02 09:25:00 +0900

    C# 클래스,  네임 스페이스

--- HelloWorl/Program.cs
+++ HelloWorl/Program.cs
@@ -1,71 +1,23 @@
-﻿/*
-Console.WriteLine("Hello, World!");
-
-
-// ==== 단원 과제1 ====
-string pangram = "The quick brown fox jumps over the lazy dog";
-
-string[] message = pangram.Split(' ');
-string[] newMessage = new string[message.Length];
-
-for (int i = 0; i < message.Length; i++)
-{
-    char[] letters = message[i].ToCharArray();
-    Array.Reverse(letters);
-    newMessage[i] = new string(letters);
-}
-
-string result = String.Join(" ", newMessage);
-Console.WriteLine(result);
-
-// ===== 단원 과제2 =====
-string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
-string[] items = orderStream.Split(',');
-
-foreach (var item in items)
-{
-    if (item.StartsWith("B"))
-    {
-        Console.WriteLine(item);
-    }
-}
-*/
-
-/* string[] stringArr = { "wan", "12", "young", "154", "13" };
-int sum = 0;
-string name = "";
-foreach(string value in stringArr)
-{
-    if (int.TryParse(value, out int result)) 
-    {
-        sum += result;
-    }
-    else
-    {
-        name += value;
-    }
-}
-
-Console.WriteLine(name);
-Console.WriteLine(sum);
-*/
-
 using System;
+using MyNewApp.Utilites;
 
 namespace MyNewApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(String[] args)
         {
             string value = "Microsoft Learn";
             string reversedValue = Utility.Reverse(value);
             Console.WriteLine($"Secret message: {reversedValue}");
         }
     }
+}
 
-     class Utility
-     {
+namespace MyNewApp.Utilities
+{
+    class Utility
+    {
         public static string Reverse(string message)
         {
             char[] letters = message.ToCharArray();
@@ -74,3 +26,4 @@ namespace MyNewApp
         }
     }
 }
+
